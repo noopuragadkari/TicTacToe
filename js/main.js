0e021ca8a2d916c9c321360239fcb552878bfb7a
noopuragadkari <noopuragadkari@gmail.com>
2022-05-07 13:48:58 -0700

    Code added

--- js/main.js
+++ js/main.js
@@ -1,83 +1,120 @@
-//Define a colors object
-const colorsObj = {
-null:'#999999',
-Player1: '#ff9999',
-PlayerO:'#00ffbf'
-};
 
-//Define the 8 possible winning combinations
-const winningConditions = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
- ];
- let board, turn, winner;
- const boardSquares = document.querySelectorAll('.box');
- const restartButton = document.querySelector('#restartButton');
+  
 
- const message = document.querySelector('h1');
-const accessBoard = document.querySelector('#board');
+  
+window.addEventListener('DOMContentLoaded', () => {
+  const tiles = Array.from(document.querySelectorAll('.tile'));
+  const playerDisplay = document.querySelector('.display-player');
+  const resetButton = document.querySelector('#reset');
+  const announcer = document.querySelector('.announcer');
 
- //Upon loading the app should.....To start game 
- restartButton.addEventListener('click',startGame);
- accessBoard.addEventListener('click', playerClick);
+  let board = ['', '', '', '', '', '', '', '', ''];
+  let currentPlayer = 'X';
+  let isGameActive = true;
 
- function startGame(){
-    //Initialize the board array to 9 nulls to represent empty squares.
-    board = [null, null, null, null, null, null, null, null, null];
-    //Initialize whose turn it is to (Player1)1 . will be represented by (PlayerO)-1.
-    let Player1 = 1;
-    let PlayerO = -1;
-    //Initialize winner to null to represent that there is no winner or tie yet.
-    winner = null;
-    //Render the board:
-    render();
- }
- function render(){
-  //Loop over each of the 9 elements that represent the squares on the page
-  board.forEach(myFunction);
-  function myFunction(boardSquare,index){
-      //Use the index of the iteration to access the mapped value from the board array.
-      //Set the background color of the current element by using the value as a key on the colors lookup object (constant).
-    boardSquares[index].style.background = colorsObj[boardSquare];
+  const PLAYERX_WON = 'PLAYERX_WON';
+  const PLAYERO_WON = 'PLAYERO_WON';
+  const TIE = 'TIE';
+
+  const winningConditions = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+  ];
+
+  function handleResultValidation() {
+      let roundWon = false;
+      for (let i = 0; i <= 7; i++) {
+          const winCondition = winningConditions[i];
+          const a = board[winCondition[0]];
+          const b = board[winCondition[1]];
+          const c = board[winCondition[2]];
+          if (a === '' || b === '' || c === '') {
+              continue;
+          }
+          if (a === b && b === c) {
+              roundWon = true;
+              break;
+          }
+      }
+
+  if (roundWon) {
+          announce(currentPlayer === 'X' ? PLAYERX_WON : PLAYERO_WON);
+          isGameActive = false;
+          return;
+      }
+
+  if (!board.includes(''))
+      announce(TIE);
   }
-  // Render a message:
-  // If winner has a value other than null (game still in progress)
-  if(winner!==null){
-    message.innerHTML = 'Game still in progress.......';
-  }
-  //If winner is equal to 'T' (tie), render a tie message.
-  else if(winner === 'T'){
-    message.innerHTML = 'Its a tie...............'
-  }
-  //Otherwise, render a congratulatory message to which player has won - use the color name for the player.
-  else{
-    message.innerHTML = 'The winner is '+colorsObj[turn];
-  }
- }
 
- //Handle a player clicking a square:
- function playerClick(){
-     //Obtain the index of the square that was clicked
-    let elements = document.getElementsByClassName("box");
-    let idx='';
-    for(let i=0; i<elements.length; i++) {
-        idx += elements[i].id;
-    }
-    console.log(idx);
-    //If the board has a value at the index, immediately return because that square is already taken.
-	//If winner is not null, immediately return because the game is over.
-    if(board[idx] || winner!==null){
-        return;
-    }
-    // Update the board array at the index with the value of turn.
-    board[idx] = turn;
-    //Flip turns by multiplying turn by -1 (flips a 1 to -1, and vice-versa).
-    turn = turn * -1;
-    render();
-}
+  const announce = (type) => {
+      switch(type){
+          case PLAYERO_WON:
+              announcer.innerHTML = 'Player <span class="playerO">O</span> Won';
+              break;
+          case PLAYERX_WON:
+              announcer.innerHTML = 'Player <span class="playerX">X</span> Won';
+              break;
+          case TIE:
+              announcer.innerText = 'Tie';
+      }
+      announcer.classList.remove('hide');
+  };
+
+  const isValidAction = (tile) => {
+      if (tile.innerText === 'X' || tile.innerText === 'O'){
+          return false;
+      }
+
+      return true;
+  };
+
+  const updateBoard =  (index) => {
+      board[index] = currentPlayer;
+  }
+
+  const changePlayer = () => {
+      playerDisplay.classList.remove(`player${currentPlayer}`);
+      currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+      playerDisplay.innerText = currentPlayer;
+      playerDisplay.classList.add(`player${currentPlayer}`);
+  }
+
+  const userAction = (tile, index) => {
+      if(isValidAction(tile) && isGameActive) {
+          tile.innerText = currentPlayer;
+          tile.classList.add(`player${currentPlayer}`);
+          updateBoard(index);
+          handleResultValidation();
+          changePlayer();
+      }
+  }
+  
+  const resetBoard = () => {
+      board = ['', '', '', '', '', '', '', '', ''];
+      isGameActive = true;
+      announcer.classList.add('hide');
+
+      if (currentPlayer === 'O') {
+          changePlayer();
+      }
+
+      tiles.forEach(tile => {
+          tile.innerText = '';
+          tile.classList.remove('playerX');
+          tile.classList.remove('playerO');
+      });
+  }
+
+  tiles.forEach( (tile, index) => {
+      tile.addEventListener('click', () => userAction(tile, index));
+  });
+
+  resetButton.addEventListener('click', resetBoard);
+});
